2c1ea4af683b13be8d5b808e1e1a9a703b4ce790
thiagoaxll <thiagocnpf@gmail.com>
2016-04-27 13:59:55 -0300

    Added files via upload

--- Jogo/Jogadas.py
+++ Jogo/Jogadas.py
@@ -2,8 +2,10 @@ from Dado import Dado
 from random import randint
 
 
-class Jogadas(object):
 
+class Jogadas(object):
+    nome1 = None
+    nome2 = None
     dado = Dado()
     def atkatk(self,vida1,vida2,base1,base2):
         atk1 = self.dado.aleatorio()
@@ -15,15 +17,19 @@ class Jogadas(object):
             atk1 = base1
         if atk2 > base2:
             atk2 = base2
+
         if atk1 == 1:
-            print("Jogador 1 Crítico")
+            print(self.nome1,"Crítico")
             atk1 = base1 * 2
         if atk2 == 1:
-            print("Jogador 2 Crítico")
+            print(self.nome2,"Crítico")
             atk2 = base2 * 2
 
 
-        print("\nAtaque jogador1:",atk1,"|| Ataque jogador2:",atk2,"\n")
+
+
+
+        print("\nAtaque:",self.nome1," ",atk1,"|| Ataque:",self.nome2," ",atk2,"\n")
         vida2 -= atk1
         vida1 -= atk2
 
@@ -32,11 +38,15 @@ class Jogadas(object):
 
     def atkdef(self,vida2,base1,def2):
         atk1 = self.dado.aleatorio()
-        print("Valor do dado de Ataque:",atk1)
+        print("Valor do dado de Ataque:", atk1, "Valor da Defesa:", def2)
         if atk1 == 1:
-            print("Jogador 1 Crítico")
+            print(self.nome1, "Crítico")
             atk1 = base1 * 2
             vida2 = vida2-atk1
+            print("----------------------------------")
+            print(self.nome1,"Crítico")
+            print("Valor do dado de Ataque:", atk1, "Valor da Defesa:", def2)
+            print("----------------------------------")
             return vida2
 
         if atk1 > base1:
@@ -44,13 +54,13 @@ class Jogadas(object):
 
         if atk1 > def2:
             print("--------------------------------------")
-            print("Jogador 2 Recebeu dano.")
+            print(self.nome2," Recebeu dano.")
             print("---------------------------------------")
             atk1 = atk1 - def2
             vida2 -= atk1
             return vida2
         print("----------------------------------")
-        print("Jogador 2 Não recebeu dano")
+        print(self.nome2,"Não recebeu dano")
         print("---------------------------------")
         return vida2
 
@@ -59,10 +69,10 @@ class Jogadas(object):
         atk2 = self.dado.aleatorio()
         print("Valor do dado de Ataque:", atk2,"Valor da Defesa:",def1)
         if atk2 == 1:
-            atk2 = base1 * 2
+            atk2 = base2 * 2
             vida1 = vida1 - atk2
             print("----------------------------------")
-            print("Jogador 2 Crítico")
+            print(self.nome2,"Crítico")
             print("Valor do dado de Ataque:", atk2, "Valor da Defesa:", def1)
             print("----------------------------------")
 
@@ -73,13 +83,13 @@ class Jogadas(object):
 
         if atk2 > def1:
             print("----------------------------------")
-            print("Jogador 1 Recebeu dano.")
+            print(self.nome1,"Recebeu dano.")
             print("----------------------------------")
             atk2 = atk2 - def1
             vida1 -= atk2
             return vida1
         print("----------------------------------")
-        print("Jogador 1 Não recebeu dano")
+        print(self.nome1,"Não recebeu dano")
         print("----------------------------------")
         return vida1
 
@@ -128,16 +138,21 @@ class Jogadas(object):
         atk1 = self.dado.aleatorio()
         atk2 = self.dado.aleatorio()
 
-        print("dado 1 ||", atk1)
-        print("dado 2 ||", atk2)
+        print("Dado 1 ||", atk1)
+        print("Dado 2 ||", atk2)
         if atk1 < base1:
             vida2 -= base1
+            print(self.nome1,"Dano:",base1)
         else:
             vida2 -= atk1
+            print(self.nome2,"Dano:", atk1)
         if atk2 < base2:
             vida1 -= base2
+            print(self.nome2,"Dano:", base2)
+
         else:
             vida1 -= atk2
+            print(self.nome1,"Dano:", atk2)
 
         return vida1,0,vida2,0
 
--- Jogo/Jogo.py
+++ Jogo/Jogo.py
@@ -12,19 +12,24 @@ jogador2 = [0,0,100,0]
 
 nome1 = nome.nomes[0]
 nome2 = nome.nomes[1]
-'''
+play.nome1 = nome1
+play.nome2 = nome2
+
 c = ['|','|','|','|','|','|','|','|','|','|']
-print("Gerando atributos: ")
+
+
+
+print("Gerando atributos basico ")
 
 for i in range(10):
     print(c[i])
     time.sleep(0.4)
 
 print("Atributos Gerados.")
-'''
+
+
 def mensagem():
-    print("um dos jogadores não possui pontos de especial.")
-    print("Escolha novamente sua ação")
+    print("um dos jogadores não possui pontos de especial.\nEscolha novamente sua ação")
 
 
 # Gera o atributo basico dos Jogadores.
@@ -49,15 +54,22 @@ print(" ----------------------------------")
 controle = True
 turno = 1
 while controle == True:
+    if jogador1[3] > 5:
+        jogador1[3] = 5
+    if jogador2[3] > 5:
+        jogador2[3] = 5
     print("--------------------------------------------------")
     print("",nome1,"HP:",jogador1[2],"     Pontos:",jogador1[3],"\n",nome2,"HP:",jogador2[2],"     Pontos:",jogador2[3])
     print("--------------------------------------------------")
 
+
+    print("\n\n\n\n")
+
     print("**************************************************")
     print("Rodada:", turno,"\n")
-    print("Jogador1:")
+    print(str(nome1)+":")
     acao1 = input("Digite sua ação: A, D ou S: ")
-    print("\nJogador2:")
+    print("\n"+str(nome2) + ":")
     acao2 = input("Digite sua ação: A, D ou S: ")
     print("**************************************************\n")
 
@@ -72,7 +84,6 @@ while controle == True:
 
     if acao1 == 'a' and acao2 == 'd':
         jogador2[2] = play.atkdef(jogador2[2],jogador1[0],jogador2[1])
-        print(nome1, "Vida:", jogador1[2], nome2, "Vida:", jogador2[2])
         jogador1[3] += 1
         jogador2[3] += 2
 
@@ -80,36 +91,42 @@ while controle == True:
 
     if acao1 == 'd' and acao2 == 'a':
         jogador1[2] = play.defatk(jogador1[2], jogador2[0], jogador1[1])
+        jogador1[3] += 2
+        jogador2[3] += 1
 
 
     if acao1 == 'd' and acao2 == 'd':
         play.defdef()
-        jogador1[3] += 2
-        jogador2[3] += 2
         print("\n------------------------------------------------------")
 
 
     if acao1 == 's' and acao2 == 'a':
-        if jogador1[3] == 5:
+        if jogador1[3] >= 5:
             jogador2[3] += 1
             jogador1[2],jogador1[3],jogador2[2] = play.s1 (jogador1[0],jogador1[2],jogador2[0],jogador2[2])
         else:
             mensagem()
     if acao1 == 'a' and acao2 == 's':
-        if jogador2[3] == 5:
+        if jogador2[3] >=  5:
             jogador2[2], jogador2[3], jogador1[2] = play.s2(jogador2[0], jogador2[2], jogador1[0], jogador1[2])
             jogador1[3] += 1
         else:
             mensagem()
 
     if acao1 == 's' and acao2 == 's':
-        if jogador1[3] == 5 and jogador2[3] == 5:
+        if jogador1[3] >= 5 and jogador2[3] >= 5:
             jogador1[2], jogador1[3], jogador2[2], jogador2[3] = play.s1s2(jogador1[0], jogador1[2], jogador2[0], jogador2[2])
         else:
             mensagem()
 
+    if acao1 == "s" and acao2 == "d":
+        print("Não é possivel defender o ataque especial, digite sua ação novamente.")
 
-    if jogador1[2] <= 0 and jogador2[2]:
+    if acao1 == "d" and acao2 == "s":
+        print("Não é possivel defender o ataque especial, digite sua ação novamente.")
+
+
+    if jogador1[2] <= 0 and jogador2[2] <= 0:
         print("Empate")
         controle = False
     elif jogador1[2] <= 0:
@@ -120,10 +137,10 @@ while controle == True:
         controle = False
 
     turno += 1
-    print("\n\n\n\n\n\n\n\n")
+
 
 print("\n\n\n\n\n\n\n\n")
-print("FIM")
+print("Game Over")
 
 
 
